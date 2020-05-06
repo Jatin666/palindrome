cdf6feb8895c36835ae240c69f70301a705d7b61
Jatin666 <59907081+Jatin666@users.noreply.github.com>
2020-05-06 14:17:18 +0530

    Initial commit

---
+++ palin.py
@@ -0,0 +1,6 @@
+print("Welcome to Palindrome")
+user_input=input('enter a string : ')
+if(user_input==user_input[:: - 1]):
+     print("it is a palindrome")
+else:
+      print("it is not a palindrome")
